64ac33b6f83600dbfb5fd0f390e4090e96e206d6
techmaster <cuong@techmaster.vn>
2017-07-21 10:10:40 +0700

    SoChinhPhuong

--- GiaiBaiTap/main.c
+++ GiaiBaiTap/main.c
@@ -1,19 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 //001
-void soGapDoi() {
-	int N;
-	scanf("%d", &N);
-	printf("%d * 2 = %d", N, N * 2);
+void soGapDoi()
+{
+    int N;
+    scanf("%d", &N);
+    printf("%d * 2 = %d", N, N * 2);
 }
 
 //002
-void phepTinh1() {
+void phepTinh1()
+{
+
+}
+
+void kiemTraSoChinhPhuong()
+{
+    int N;
+
+    while (1)
+    {
+        printf("Nhap vao mot so de kiem tra. Nhap 0 de thoat\n");
+        scanf("%d", &N);
+        if (N == 0) return;
+        int laSoChinhPhuong = 0;
+        for (int i = 1; i <= N/2; i++)
+        {
+            if (N == i * i)
+            {
+                laSoChinhPhuong = 1;
+                break;
+            }
+        }
+
+        if (laSoChinhPhuong)
+        {
+            printf("%d la so chinh phuong\n\n", N);
+        }
+        else
+        {
+            printf("%d khong phai la so chinh phuong\n\n", N);
+        }
+    }
 
 }
 
 int main()
 {
-		soGapDoi();
+    //soGapDoi();
+    kiemTraSoChinhPhuong();
     return 0;
 }
