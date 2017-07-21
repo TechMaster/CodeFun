1bfa86d49f89ebc04f7964a01fb155c876333bef
techmaster <cuong@techmaster.vn>
2017-07-21 09:49:16 +0700

    GiaiBaiTap

---
+++ GiaiBaiTap/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+//001
+void soGapDoi() {
+	int N;
+	scanf("%d", &N);
+	printf("%d * 2 = %d", N, N * 2);
+}
+
+//002
+void phepTinh1() {
+
+}
+
+int main()
+{
+		soGapDoi();
+    return 0;
+}
