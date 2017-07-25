42345a64405ac82bce1fd179a442a9a108c619a8
techmaster <cuong@techmaster.vn>
2017-07-25 10:20:57 +0700

    DaoMang2

--- DemoArray/main.c
+++ DemoArray/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 void daoMang() {
-	int arr[20] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};  //Mang co kich thuoc co dinh
+	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};  //Mang co kich thuoc co dinh
     int N = sizeof(arr)/sizeof(arr[0]);
 
     //printf("size of one elem")
@@ -26,13 +26,34 @@ co the dung atoi hoac strol
 http://en.cppreference.com/w/c/string/byte/strtol
 */
 void daoMang2() {
+	int arr[100];
+	int N = 0;
 	char inputText[8];
-
+	//Nhap chuoi, chuyen doi sang so, neu chuyen doi thanh cong thi them vao mang arr
 	while (1) {
 			gets(inputText);
 			if (inputText[0] == 'q') break;
-			int i = atoi (inputText);
-			printf("%d\n", i);
+			char * temp;
+			int _val = strtol(inputText, &temp, 10);
+			if (temp[0] == '\0') { //convert successfully
+        arr[N] = _val;
+        N++;
+        if (N == sizeof(arr) / sizeof(arr[0])) {
+          break;
+        }
+			}
+	}
+
+	//Dao mang
+	for (int i = 0; i <= N/2; i++) {
+			int temp = arr[i];
+			arr[i] = arr[N - 1 - i];
+			arr[N - 1 - i] = temp;
+	}
+
+	//In ra ket qua
+	for (int i = 0; i < N; i++) {
+		printf("%d\n", arr[i]);
 	}
 
 }
